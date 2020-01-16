531da9930c88ab54d103d0d773659fbd60f38f6f
joel <joelcote68@gmail.com>
2020-01-16 10:55:39 -0600

    adding folder admins

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "billing_iam_admins" {
   source          = "./modules/billing_iam_bindings"
 }
+
+module "folder_iam_admins" {
+  source          = "./modules/folder_iam_bindings"
+}
--- modules/folder_iam_bindings/main.tf
+++ modules/folder_iam_bindings/main.tf
@@ -2,33 +2,65 @@ module "folder_iam_bindings" {
    source = "terraform-google-modules/iam/google//modules/folders_iam"
    version = "~> 3.0"
    folders = [
-      "1017394418635 "
+      "1017394418635"
    ]
    mode = "additive"
    bindings = {
 
       "roles/viewer" = [
-         "group:afrl-developers@afrldigitalmfg.org "
+         "group:afrl-developers@afrldigitalmfg.org"
       ],
 
       "roles/cloudsql.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org "
+         "group:afrl-developers@afrldigitalmfg.org"
       ],
 
       "roles/spanner.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org "
+         "group:afrl-developers@afrldigitalmfg.org"
       ],
 
       "roles/bigtable.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org "
+         "group:afrl-developers@afrldigitalmfg.org"
       ],
 
       "roles/firebasedatabase.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org "
+         "group:afrl-developers@afrldigitalmfg.org"
       ],
 
       "roles/redis.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org "
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+        "roles/compute.admin" = [
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+      "roles/iam.serviceAccountUser" = [
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+      "roles/compute.viewer" = [
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+      "roles/logging.viewer" = [
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+      "roles/monitoring.admin" = [
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+      "roles/storage.admin" = [
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+      "roles/dns.reader" = [
+         "group:afrl-developers@afrldigitalmfg.org"
+      ],
+
+      "roles/compute.networkViewer" = [
+         "group:afrl-developers@afrldigitalmfg.org"
       ],
    }
 }
