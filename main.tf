4c41d69088365995be11ac29eecab3ea9322b788
joel <joelcote68@gmail.com>
2020-01-16 11:21:20 -0600

    testing

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ module "folder_iam_bindings" {
   source          = "./modules/folder_iam_bindings"
 }
 
-module "network_iam_bindings" {
-  source          = "./modules/network_admin_bindings"
-}
+//module "network_iam_bindings" {
+//  source          = "./modules/network_admin_bindings"
+//}
 
-module "security_iam_bindings" {
-  source          = "./modules/security_admin_bindings"
-}
+//module "security_iam_bindings" {
+//  source          = "./modules/security_admin_bindings"
+//}
 
-module "org_iam_bindings" {
-  source          = "./modules/network_admin_bindings"
-}
+//module "org_iam_bindings" {
+//  source          = "./modules/network_admin_bindings"
+//}
