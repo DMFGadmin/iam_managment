9a2f6ba43ff1d79906279352e96966a5dc9d83cd
joel <joelcote68@gmail.com>
2020-01-16 11:15:55 -0600

    adding security and network admin bindings

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
-module "billing_iam_admins" {
+module "billing_iam_bindings" {
   source          = "./modules/billing_iam_bindings"
 }
 
-module "folder_iam_admins" {
+module "folder_iam_bindings" {
   source          = "./modules/folder_iam_bindings"
+}
+
+module "network_iam_bindings" {
+  source          = "./modules/network_admin_bindings"
+}
+
+module "security_iam_bindings" {
+  source          = "./modules/security_admin_bindings"
+}
+
+module "org_iam_bindings" {
+  source          = "./modules/network_admin_bindings"
 }
