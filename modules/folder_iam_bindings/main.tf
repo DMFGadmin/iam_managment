2eb2dee6c38a9f27fb426b422b46e40158d3d6ab
joel <joelcote68@gmail.com>
2020-01-28 13:48:14 -0600

    dumping module

--- modules/folder_iam_bindings/main.tf
+++ modules/folder_iam_bindings/main.tf
@@ -1,85 +1,130 @@
-module "folder_iam_bindings" {
-   source = "terraform-google-modules/iam/google//modules/folders_iam"
-   version = "~> 3.0"
-   folders = [
-      "${var.afrl_bd_folder_id}"
+resource "google_folder_iam_binding" "viewer" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/viewer"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
    ]
-   mode = "additive"
-   bindings = {
+}
 
-      "roles/viewer" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "cloudsql_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/cloudsql.admin"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/cloudsql.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "spanner_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/spanner.admin"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/spanner.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "bigtable_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/bigtable.admin"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/bigtable.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "redis_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/redis.admin"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/firebasedatabase.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "compute_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/compute.admin"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/redis.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "service_account_user" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/iam.serviceAccountUser"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org",
+      "group:afrl-network-admins@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/compute.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "logging_viewer" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/logging.viewer"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org",
+      "group:afrl-network-admins@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/iam.serviceAccountUser" = [
-         "group:afrl-developers@afrldigitalmfg.org",
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "monitoring_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/monitoring.admin"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org",
+      "group:afrl-network-admins@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/logging.viewer" = [
-         "group:afrl-developers@afrldigitalmfg.org",
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "storage_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/storage.admin"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/monitoring.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org",
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "dns_reader" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/dns.reader"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/storage.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "compute_network_viewer" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/compute.networkViewer"
+   members = [
+   "group:afrl-developers@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/dns.reader" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "network_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/compute.networkAdmin"
+   members = [
+   "group:afrl-network-admins@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/compute.networkViewer" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "security_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/compute.securityAdmin"
+   members = [
+   "group:afrl-network-admins@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/compute.networkAdmin" = [
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
+resource "google_folder_iam_binding" "shared_vpc_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/compute.xpnAdmin"
+   members = [
+   "group:afrl-network-admins@afrldigitalmfg.org"
+   ]
+}
 
-      "roles/compute.securityAdmin" = [
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
-
-      "roles/compute.xpnAdmin" = [
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
-
-      "roles/project.owner" = [
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
-
-      "roles/dns.admin" = [
-         "group:afrl-network-admins@afrldigitalmfg.org"
-      ],
-   }
+resource "google_folder_iam_binding" "dns_admin" {
+   folder   =  var.afrl_bd_folder_id
+   role  = "roles/dns.admin"
+   members = [
+   "group:afrl-network-admins@afrldigitalmfg.org"
+   ]
 }
