eb3eacb31ab705ee49a1af03ec0ea84d80d87dcd
joel <joelcote68@gmail.com>
2020-01-28 13:15:32 -0600

    updating folder permissions

--- modules/folder_iam_bindings/main.tf
+++ modules/folder_iam_bindings/main.tf
@@ -36,19 +36,18 @@ module "folder_iam_bindings" {
       ],
 
       "roles/iam.serviceAccountUser" = [
-         "group:afrl-developers@afrldigitalmfg.org"
-      ],
-
-      "roles/compute.viewer" = [
-         "group:afrl-developers@afrldigitalmfg.org"
+         "group:afrl-developers@afrldigitalmfg.org",
+         "group:afrl-network-admins@afrldigitalmfg.org"
       ],
 
       "roles/logging.viewer" = [
-         "group:afrl-developers@afrldigitalmfg.org"
+         "group:afrl-developers@afrldigitalmfg.org",
+         "group:afrl-network-admins@afrldigitalmfg.org"
       ],
 
       "roles/monitoring.admin" = [
-         "group:afrl-developers@afrldigitalmfg.org"
+         "group:afrl-developers@afrldigitalmfg.org",
+         "group:afrl-network-admins@afrldigitalmfg.org"
       ],
 
       "roles/storage.admin" = [
@@ -62,6 +61,27 @@ module "folder_iam_bindings" {
       "roles/compute.networkViewer" = [
          "group:afrl-developers@afrldigitalmfg.org"
       ],
+
+       "roles/compute.networkAdmin" = [
+         "group:afrl-network-admins@afrldigitalmfg.org"
+      ],
+
+      "roles/compute.securityAdmin" = [
+         "group:afrl-network-admins@afrldigitalmfg.org"
+      ],
+
+      "roles/compute.xpnAdmin" = [
+         "group:afrl-network-admins@afrldigitalmfg.org"
+      ],
+
+      "roles/project.owner" = [
+         "group:afrl-network-admins@afrldigitalmfg.org"
+      ],
+
+      "roles/dns.admin" = [
+         "group:afrl-network-admins@afrldigitalmfg.org"
+      ],
+   }
 
    }
 }
